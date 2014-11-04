e800c5ec9430d5f6a4e19b82eb8b52ce404fa128
Boudewijn Schoon <boudewijn@zicht.nl>
2014-11-04 13:02:34 +0000

    adds function MenuManager::getItemBy which will replace MenuManager::getItem
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtMenuBundle/trunk@49313 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Bundle/MenuBundle/Manager/MenuManager.php
+++ src/Zicht/Bundle/MenuBundle/Manager/MenuManager.php
@@ -7,9 +7,13 @@
 namespace Zicht\Bundle\MenuBundle\Manager;
 
 use \Doctrine\Bundle\DoctrineBundle\Registry;
+use \Zicht\Bundle\MenuBundle\Entity\MenuItem;
 
 class MenuManager
 {
+    /**
+     * @param Registry $doctrine
+     */
     function __construct(Registry $doctrine)
     {
         $this->doctrine = $doctrine;
@@ -18,25 +22,37 @@ class MenuManager
     }
 
 
+    /**
+     * @param MenuItem $item
+     */
     function addItem(\Zicht\Bundle\MenuBundle\Entity\MenuItem $item)
     {
         $this->items[]= $item;
     }
 
 
+    /**
+     * @param MenuItem $item
+     */
     function removeItem(\Zicht\Bundle\MenuBundle\Entity\MenuItem $item)
     {
         $this->remove[]= $item;
     }
 
 
-    function flush()
+    /**
+     * @param bool $flush
+     */
+    function flush($flush = false)
     {
         foreach ($this->items as $item) {
             $this->doctrine->getManager()->persist($item);
         }
         foreach ($this->remove as $item) {
             $this->doctrine->getManager()->remove($item);
+        }
+        if ($flush) {
+            $this->doctrine->getManager()->flush();
         }
     }
 
@@ -46,9 +62,46 @@ class MenuManager
      *
      * @param string $path
      * @return \Zicht\Bundle\MenuBundle\Entity\MenuItem
+     * @deprecated Use getItemBy(array(':path' => $path))
      */
     public function getItem($path)
     {
         return $this->doctrine->getManager()->getRepository('ZichtMenuBundle:MenuItem')->findOneByPath($path);
+    }
+
+    /**
+     * @param array $parameters Array containing keys ':name' or ':path'
+     * @param MenuItem $ancestor Optional MenuItem whose children will be searched
+     * @return \Zicht\Bundle\MenuBundle\Entity\MenuItem
+     */
+    public function getItemBy(array $parameters, MenuItem $ancestor=null)
+    {
+        $where = array();
+        foreach ($parameters as $key=>$value) {
+            switch ($key) {
+                case ':name':
+                    $where [] = 'm.name = :name';
+                    break;
+                case ':path':
+                    $where [] = 'm.path = :path';
+                    break;
+                default:
+                    throw new \Exception("Unsupported parameter [$key].");
+                    break;
+            }
+        }
+
+        if (!is_null($ancestor)) {
+            $where [] = 'm.lft > :lft';
+            $where [] = 'm.rgt < :rgt';
+            $parameters[':lft'] = $ancestor->getLft();
+            $parameters[':rgt'] = $ancestor->getRgt();
+        }
+
+        /** @var \Doctrine\Orm\Query $query */
+        $query = $this->doctrine->getManager()->createQuery(join(' ', array('SELECT m FROM ZichtMenuBundle:MenuItem m WHERE', join(' AND ', $where))));
+        $query->setParameters($parameters);
+        $query->setMaxResults(1);
+        return current($query->getResult());
     }
 }

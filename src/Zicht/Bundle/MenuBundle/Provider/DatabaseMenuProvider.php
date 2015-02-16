964670ae519f08febaefcc567be9ed621c4700f6
Joppe Aarts <joppe@zicht.nl>
2015-02-16 10:29:43 +0000

    database menu provider added
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtMenuBundle/trunk@56456 0f35748b-8228-47f0-bbea-889afbbd9a42

---
+++ src/Zicht/Bundle/MenuBundle/Provider/DatabaseMenuProvider.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @author Joppe Aarts <joppe@zicht.nl>
+ * @copyright Zicht Online <http://zicht.nl>
+ */
+
+namespace Zicht\Bundle\MenuBundle\Menu\Provider;
+
+use \Knp\Menu\Provider\MenuProviderInterface;
+use \Symfony\Component\DependencyInjection\ContainerInterface;
+use \Zicht\Bundle\MenuBundle\Menu\Builder;
+
+/**
+ * Class DatabaseMenuProvider
+ *
+ * @package Zicht\Bundle\BhicCoreBundle\Provider
+ */
+class DatabaseMenuProvider implements MenuProviderInterface
+{
+    /**
+     * @var Builder
+     */
+    protected $builder = null;
+
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * Constructor
+     *
+     * @param Builder $builder
+     * @param ContainerInterface $container
+     */
+    public function __construct(Builder $builder, ContainerInterface $container)
+    {
+        $this->builder = $builder;
+        $this->container = $container;
+    }
+
+    /**
+     * Retrieves a menu by its name
+     *
+     * @param string $name
+     * @param array $options
+     * @return \Knp\Menu\ItemInterface
+     * @throws \InvalidArgumentException if the menu does not exists
+     */
+    public function get($name, array $options = array())
+    {
+        return $this->builder->build($name, $this->container->get('request'));
+    }
+
+    /**
+     * Checks whether a menu exists in this provider
+     *
+     * @param string $name
+     * @param array $options
+     * @return bool
+     */
+    public function has($name, array $options = array())
+    {
+        $root = $this->builder->getRootItemByName($name, $this->container->get('request'));
+
+        return null !== $root;
+    }
+}

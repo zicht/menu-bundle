fda43b72396e5183475080a17cea472aafdc0f54
Erik Trapman <erik@zicht.nl>
2020-09-21 09:26:07 +0200

    Upd MenuItem admin with language and name

---
+++ src/Admin/Extension/TreeAdminExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Zicht\Bundle\MenuBundle\Admin\Extension;
+
+use Sonata\AdminBundle\Admin\AbstractAdminExtension;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class TreeAdminExtension extends AbstractAdminExtension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function configureFormFields(FormMapper $formMapper)
+    {
+        $subject = $formMapper->getAdmin()->getSubject();
+        // follow the path from Zicht\Bundle\AdminBundle\Admin\TreeAdmin
+        $formMapper
+            ->tab('General')
+            ->with('General')
+                ->add('language', TextType::class, ['required' => false, 'attr' => ['placeholder' => 'nl']])
+                ->add('name', TextType::class, ['required' => false, 'help' => 'admin.help.menu_item_name', 'disabled' => $subject && $subject->getId()])
+            ->end()
+            ->end();
+    }
+}
